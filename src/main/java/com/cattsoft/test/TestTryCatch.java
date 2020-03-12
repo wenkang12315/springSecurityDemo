db24993455a5d58e5f7e8843b67e50f58bc6d98d
wenkang12315 <wenkang12315@163.com>
2020-03-12 10:51:46 +0800

    提交测试深拷贝浅拷贝代码   提交部分面试摘要分析

--- src/main/java/com/cattsoft/test/TestTryCatch.java
+++ src/main/java/com/cattsoft/test/TestTryCatch.java
@@ -1,10 +1,19 @@
 package com.cattsoft.test;
 
+import com.cattsoft.eitity.test.TbUser;
+import org.aspectj.weaver.ast.Test;
+import org.omg.CORBA.portable.ValueOutputStream;
+
 public class TestTryCatch {
     public static void main(String[] args) {
-        System.out.println(getNum());
+       // System.out.println(getNum());
+        testClone();
     }
 
+    /**
+     * 测试finally
+     * @return
+     */
     public static int getNum() {
         try {
             int a = 1 / 0;
@@ -14,6 +23,52 @@ public class TestTryCatch {
         } finally {
             return 3;
         }
+    }
 
+    /**
+     * 测试深拷贝 浅拷贝  针对clone方法
+     */
+    public static void testClone(){
+        Person p1 = new Person(18,"张三");
+        try {
+            Person p2 = (Person)p1.clone();
+            //System.out.println(p1==p2);
+            System.out.println(p1.getName()==p2.getName());//属性为浅拷贝
+            //p2.setName("李四");
+            //System.out.println(p1.getName());
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class Person implements Cloneable{
+    private int age;
+    private String name;
+
+    public Person(int age,String name){
+        this.age=age;
+        this.name=name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
     }
 }
